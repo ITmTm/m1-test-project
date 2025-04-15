560e3d03f236839c2dc5a061eced707e1308e2f8
Timur <Stafr95@gmail.com>
2025-04-15 17:02:06 +0500

    Architectural change to the file system

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './pages/components/app/App';
+import App from './app/App';
 
 import './style/styles.scss';
 
