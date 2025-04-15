718a46e1416ff3befbe0b959df42b66ccdb21f98
Timur <Stafr95@gmail.com>
2025-04-15 12:23:15 +0500

    Typing Item and props, following a link, button display logic

---
+++ client/src/pages/components/listItem/ListItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../button/Button';
+
+import './listItem.scss';
+
+interface Item {
+	id: number;
+	name: string;
+	description: string;
+}
+
+interface ListItemProps {
+	item: Item;
+	isActive: boolean;
+	onSetActive: () => void
+}
+
+const ListItem: React.FC<ListItemProps> = ({ item, isActive, onSetActive }) => {
+	return (
+		<li className={isActive ? 'list-item active' : 'list-item'}>
+			<div className='list-item-actions'>
+				<div className='list-item-id'>ID: <b>{item.id}</b></div>
+
+				<Button
+					onClick={onSetActive}
+					id={item.id}
+					disabled={isActive}
+				>
+					{isActive ? 'Active' : 'Set Active'}
+				</Button>
+			</div>
+
+			<Link to={`/${item.id}`}>
+				<div>{item.name}</div>
+				<div className={'list-item__description'}>{item.description}</div>
+			</Link>
+		</li>
+	);
+};
+
+
+export default ListItem;
